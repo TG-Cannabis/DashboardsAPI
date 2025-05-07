eac3e49eb51df33c1bc3b6dffbb19a3542d7f3b5
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2025-05-06 20:26:14 -0500

    initializing project

---
+++ src/main/java/com/tgcannabis/dahsboards_api/DahsboardsApiApplication.java
@@ -0,0 +1,13 @@
+package com.tgcannabis.dahsboards_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DahsboardsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DahsboardsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tgcannabis/dahsboards_api/DahsboardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgcannabis.dahsboards_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DahsboardsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
