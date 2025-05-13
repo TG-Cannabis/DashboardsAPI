6a0221f70008bb41cfc9c864899b53a89e08e0a3
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2025-05-12 20:15:25 -0500

    feat: temporal fix for cors

--- src/main/java/com/tgcannabis/dahsboards_api/controller/SensorDataController.java
+++ src/main/java/com/tgcannabis/dahsboards_api/controller/SensorDataController.java
@@ -5,10 +5,7 @@ import com.tgcannabis.dahsboards_api.service.SensorDataService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +17,7 @@ public class SensorDataController {
     private final SensorDataService service;
 
     @GetMapping
+    @CrossOrigin
     public ResponseEntity<List<SensorData>> getSensorMeasures(
             @RequestParam(required = false) Long startDate,
             @RequestParam(required = false) Long endDate,
